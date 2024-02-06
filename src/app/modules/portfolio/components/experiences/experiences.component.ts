1e5a92a9e3419bb0e7b3216e9136caf80a95b991
ParloNeto <105455827+ParloNeto@users.noreply.github.com>
2024-02-06 19:21:20 -0300

    Improving the text descriptions

--- src/app/modules/portfolio/components/experiences/experiences.component.ts
+++ src/app/modules/portfolio/components/experiences/experiences.component.ts
@@ -15,14 +15,14 @@ export class ExperiencesComponent {
         strong: 'Estagiário em Desenvolvimento de Software',
         p: 'Serasa Experian | Ago 2023 - Present',
       },
-    text: '<p>Desde agosto de 2023, após o exato 1 ano como jovem aprendiz, passei no processo seletivo de estágio da Serasa Experian e estou atuando como desenvolvedor de software, destacando-me nas áreas Front-end com Angular, e Back-end com Java Spring Boot.</p><p>Participei de projetos de migração de APIs para a Cloud (<strong>Mainframe à AWS</strong>).</p><p>Atuei em ferramentas como: Cucumber, Cypress, Docker, Jenkins, Veracode, Kubernetes, Figma, Apache Kafka, MongoDB e MySQL.</p><p>Também irei conhecer a área de Engenharia de Dados lidando com Big Data futuramente.</p>',
+    text: '<p>Desde agosto de 2023, após exatamente um ano como jovem aprendiz, fui aprovado no processo seletivo de estágio da Serasa Experian e estou atuando como desenvolvedor de software, destacando-me nas áreas de Front-end com Angular e Back-end com Java Spring Boot.</p><p>Participei de projetos de migração de APIs para a Cloud (de Mainframe para AWS).</p><p>Trabalhei com ferramentas como: Cucumber, Cypress, Docker, Jenkins, Veracode, Kubernetes, Figma, Apache Kafka, MongoDB e MySQL.</p><p>Além disso, irei explorar a área de Engenharia de Dados, lidando com Big Data no futuro.</p>    '
     },
     {
       summary: {
         strong: 'Jovem Aprendiz em Desenvolvimento de Software',
         p: 'Serasa Experian | Ago 2022 - Jul 2023',
       },
-      text: '<p>Comecei minha jornada profissional em 2022, após concluir o programa Transforme-se, criado pela Serasa Experian na intenção de inserir jovens de baixa renda e escolaridade pública no mundo da Tecnologia. Ao entrar na empresa comecei meus estudos na linguagem Java, após 4 meses comecei a ver mais sobre o framework Spring Boot e também sobre o Angular, além de adentrar um projeto full-stack da área que somente participava jovens aprendizes e estagiários, afim de simular como são os projetos e processos do dia-a-dia de um Desenvolvedor. Nele me destaquei por em poucos meses dentro do mundo da programação, consegui realizar entregas além do esperado para um jovem aprendiz e um alto índice de aprendizado.</p>',
+      text: '<p>Comecei minha jornada profissional em 2022, após concluir o programa Transforme-se, criado pela Serasa Experian com a intenção de inserir jovens de baixa renda e escolaridade pública no mundo da Tecnologia.</p><p>Ao ingressar na empresa, iniciei meus estudos na linguagem Java. Após 4 meses, passei a explorar mais sobre o framework Spring Boot e também sobre o Angular.</p><p>Além disso, participei de um projeto full-stack, exclusivo para jovens aprendizes e estagiários, com o objetivo de simular os projetos e processos do dia-a-dia de um Desenvolvedor.</p><p>Destaquei-me ao realizar entregas além do esperado para um jovem aprendiz, evidenciando um alto índice de aprendizado em poucos meses dentro do universo da programação.</p>'
     },
   ]);
 }
