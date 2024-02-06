6c2b3d61280b9e1a7e69f9c3fefba7ae933cdfc0
ParloNeto <105455827+ParloNeto@users.noreply.github.com>
2024-02-06 10:46:30 -0300

    Creating application README and adding Fut Search Fifa project to Projects component

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -30,6 +30,20 @@ export class ProjectsComponent {
         },
       ],
     },
+    {
+      src: 'assets/img/projects/logo-fut-search.png',
+      alt: 'Fut Search Logo',
+      title: 'Fut Search Fifa',
+      width: '150px',
+      height: '50px',
+      description: '<p><strong>Fut-Search-FIFA: Criando a Sua Própria Experiência no FIFA Ultimate Team</strong></p><p>Desenvolvi o projeto <em>Fut-Search-FIFA</em> com o objetivo de proporcionar aos fãs do FIFA Ultimate Team uma experiência personalizada. Com um <em>FRONT-END</em> dinâmico em <em>Angular</em> e um <em>BACK-END</em> em <em>Spring Boot</em>, este projeto permite a criação e customização de cartas de jogadores, inspiradas diretamente no popular modo on-line da franquia FIFA, o FIFA Ultimate Team.</p><p><strong>Principais Recursos:</strong></p><ol><li><strong>Personalização Completa:</strong><ul><li>Escolha a versão do FIFA que mais lhe agrada disponíveis até o momento (FIFA 16, 17, 18, 19).</li><li>Altere nome, imagem e atributos da carta para criar o jogador dos seus sonhos.</li></ul></li><li><strong>Diversidade de Cartas Especiais:</strong><ul><li>Explore diversas variações de cartas especiais, assim como no FIFA Ultimate Team oficial.</li></ul></li><li><strong>Armazenamento Seguro:</strong><ul><li>O BACK-END em Spring Boot Java é responsável por salvar as cartas no banco de dados, garantindo a segurança e integridade das suas criações.</li></ul></li><li><strong>Busca Eficiente:</strong><ul><li>Todas as cartas criadas são exibidas na página inicial de busca de jogadores, proporcionando uma navegação fácil e rápida.</li></ul></li><li><strong>Controle Total:</strong><ul><li>Além de criar, é possível deletar as cartas, dando total controle sobre a sua coleção personalizada.</li></ul></li></ol><p><strong>Inspiração na Franquia FIFA:</strong>Este projeto foi diretamente inspirado no FIFA Ultimate Team da EA SPORTS. Assim como no modo on-line 1x1, o <em>Fut-Search-FIFA</em> permite que você crie e gerencie suas próprias cartas de jogadores, oferecendo a liberdade de personalização em até quatro versões distintas.</p><p><strong>Conecte-se com o Mundo do FIFA de Uma Maneira Única:</strong>O <em>Fut-Search-FIFA</em> não é apenas um projeto técnico, mas uma oportunidade de mergulhar ainda mais na experiência FIFA, trazendo para você a emoção de criar e colecionar jogadores personalizados.</p><p><strong>De um fã, para outros fãs.</strong></p>',
+      links: [
+        {
+          name: 'Repositório no GitHub',
+          href: 'https://github.com/ParloNeto/fifa-search',
+        },
+      ],
+    },
   ]);
 
   public openDialog(data: IProjects) {
